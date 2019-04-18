c5e1ee357e68a9cae66bc4d9fdd2ed5498926021
Xuân Tùng <xuantung.1998@gmail.com>
2019-04-18 22:56:28 +0700

    Add Song Process Bar

--- src/BotNavigation.js
+++ src/BotNavigation.js
@@ -3,6 +3,7 @@ import { Menu, Button, Icon, Progress, Segment, Grid, Label, GridColumn, Item } 
 import 'semantic-ui-css/semantic.min.css'
 
 import Sound from 'react-sound';
+import DemoSong from './DemoSong'
 
 
 export default class BotNavigation extends Component {
@@ -12,51 +13,89 @@ export default class BotNavigation extends Component {
             playStatus: Sound.status.PAUSED,
             position: 0,
             volume: 50,
-            playbackRate: 1,            
-            currentSong: {
-                url: "https://aredir.nixcdn.com/NhacCuaTui976/MuonRuouToTinh-EmilyBigDaddy-5871420_hq.mp3?st=yxHQqsuBFqj-MUVgFMPRkw&e=1555395904&download=true",
-                bytesLoaded: 1,
-                bytesTotal: 1,
-                duration: 1
-            },
-            loop: false,
-            shuffle: false
+            playbackRate: 1,
+            loop: true,
+            shuffle: false,
+            autoLoad: true,
+            currentSong: DemoSong[0],
+            bytesLoaded: 0,
+            bytesTotal: 0,
+            duration: 0
         }
     }
-    
+
     getStatusButtonText() {
         switch (this.state.playStatus) {
-          case Sound.status.PLAYING:
-            return 'pause';
-          case Sound.status.PAUSED:
-            return 'play';
-          case Sound.status.STOPPED:
-            return 'play';
-          default:
-            return 'x';
+            case Sound.status.PLAYING:
+                return 'pause';
+            case Sound.status.PAUSED:
+                return 'play';
+            case Sound.status.STOPPED:
+                return 'play';
+            default:
+                return 'x';
         }
+    }
+
+    playPrevSong = () => {
+        if (this.state.loop === true) {
+            this.setState({
+                position: 0
+            })
+        } else {
+            this.setState({
+                currentSong: DemoSong[0],
+                position: 0
+            })
+        }
+    }
+
+    playNextSong = () => {
+        if (this.state.loop === true) {
+            this.setState({
+                position: 0
+            })
+        } else {
+            this.setState({
+                currentSong: DemoSong[1],
+                position: 0
+            })
+        }
+
+    }
+
+    millisToMinutesAndSeconds = (millis) => {
+        var minutes = Math.floor(millis / 60000);
+        var seconds = ((millis % 60000) / 1000).toFixed(0);
+        return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+    }
+
+    pauseSong = () => {
+        this.setState({
+            playStatus: Sound.status.PAUSED
+        })
     }
 
     onPlaybackClicked = (e) => {
         if (this.state.playStatus === Sound.status.PLAYING) {
-            this.setState({playStatus: Sound.status.PAUSED});
+            this.setState({ playStatus: Sound.status.PAUSED });
         } else {
-            this.setState({playStatus: Sound.status.PLAYING});
+            this.setState({ playStatus: Sound.status.PLAYING });
         }
     }
 
     render() {
         return (
             <div>
-                <Sound 
-                    url="https://aredir.nixcdn.com/NhacCuaTui976/MuonRuouToTinh-EmilyBigDaddy-5871420_hq.mp3?st=yxHQqsuBFqj-MUVgFMPRkw&e=1555395904&download=true"
+                <Sound
+                    url={this.state.currentSong.url}
                     playStatus={this.state.playStatus}
                     onLoading={({ bytesLoaded, bytesTotal, duration }) => {
-                        this.setState({currentSong: {
+                        this.setState({
                             bytesLoaded: bytesLoaded,
                             bytesTotal: bytesTotal,
                             duration: duration
-                        }});
+                        })
                     }}
                     volume={this.state.volume}
                     onPlaying={({ position }) => {
@@ -74,10 +113,13 @@ export default class BotNavigation extends Component {
                     onStop={() => {
                         console.log("Song Stopped");
                     }}
-                    onFinishedPlaying={() => {
-                        this.setState({ playStatus: Sound.status.STOPPED })
+                    onFinishedPlaying={this.playNextSong}
+                    onError={(errorCode, description) => {
+                        console.log(">> Errorrrr")
+                        console.log("Error Code: " + errorCode)
+                        console.log("Description: " + description)
                     }}
-                    autoLoad="true"
+                    autoLoad={this.state.autoLoad}
                     loop={this.state.loop}
                     playbackRate={this.state.playbackRate}
                     position={this.state.position}
@@ -89,30 +131,64 @@ export default class BotNavigation extends Component {
 
                             <Grid.Column width={3} textAlign="center" stretched>
                                 <Button.Group >
-                                    <Button icon="step backward" />
-                                    <Button icon={this.getStatusButtonText()} onClick={this.onPlaybackClicked} />
-                                    <Button icon="step forward" />
-                                    <Button icon='shuffle' 
-                                            color={this.state.shuffle === true ? "orange" : "white"}
-                                            onClick={() => {
-                                                this.setState({shuffle: !this.state.shuffle})
-                                            }}
-                                            />
-                                    <Button icon='repeat' 
-                                            color={this.state.loop === true ? "orange" : "white"}
-                                            onClick={() => {
-                                                this.setState({loop: !this.state.loop})
-                                            }}
-                                            />
+
+                                    <Button basic
+                                        icon="step backward"
+                                        onClick={this.playPrevSong}
+                                    />
+
+                                    <Button basic
+                                        icon={this.getStatusButtonText()}
+                                        onClick={this.onPlaybackClicked}
+                                    />
+
+                                    <Button basic
+                                        icon="step forward"
+                                        onClick={this.playNextSong}
+                                    />
+
+                                    <Button basic
+                                        icon='shuffle'
+                                        color={this.state.shuffle === true ? "orange" : "white"}
+                                        onClick={() => {
+                                            this.setState({ shuffle: !this.state.shuffle })
+                                        }}
+                                    />
+
+                                    <Button basic
+                                        icon='repeat'
+                                        color={this.state.loop === true ? "orange" : "white"}
+                                        onClick={() => {
+                                            this.setState({ loop: !this.state.loop })
+                                        }}
+                                    />
+
                                 </Button.Group>
                             </Grid.Column>
 
-                            <Grid.Column width={8} textAlign="left" verticalAlign="middle" stretched>
-                                <Label size="medium" >Hello World</Label>
+                            <Grid.Column width={8} verticalAlign="middle" stretched>
+                                <Progress   id="Song-process"
+                                            percent={(this.state.position / this.state.duration) * 100} 
+                                            size="small" 
+                                            color="orange"
+                                            label={this.millisToMinutesAndSeconds(this.state.position)}
+                                            onClick={(e) => {
+                                                e.persist();
+                                                var offSetX = e.nativeEvent.offsetX;
+                                                var clientWidth = document.getElementById('Song-process').clientWidth;
+                                                var fraction = (offSetX / clientWidth) * this.state.duration;
+                                                this.setState({position: fraction})
+                                            }}>
+                                    {this.state.currentSong.title} - {this.state.currentSong.artist}
+                                </Progress>
+
+
                             </Grid.Column>
 
-                            <Grid.Column width={4} textAlign="center" verticalAlign="middle" stretched>
+                            <Grid.Column width={4} verticalAlign="middle" stretched>
+
                                 <Label size="medium">Hello World</Label>
+
                             </Grid.Column>
 
                         </Grid.Row>
---
+++ src/DemoSong.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        url: 'https://aredir.nixcdn.com/NhacCuaTui942/ILikeMeBetter-Lauv-4940820_hq.mp3?st=ecMNsvCayup3R3ag2CgJzw&e=1555680765&download=true',
+        title: 'I like me better',
+        artist: 'Lauv',
+        duration: 0
+    },
+    {
+        url: 'https://aredir.nixcdn.com/NhacCuaTui978/ChuyenCuaTa-GleeGiaThieu-5895245_hq.mp3?st=wYEG_eIA-VK22Fr41l17aQ&e=1555681546&download=true',
+        title: 'Chuyện của ta',
+        artist: 'Glee Gia Thiều',
+        duration: 0
+    },
+    {
+        url: 'https://raw.githubusercontent.com/scottschiller/SoundManager2/master/demo/_mp3/office_lobby.mp3',
+        title: 'office lobby',
+        artist: 'Null',
+        duration: 0
+    }
+];
