a98a57e9a5bdbb259a29ee75a94060cd49f95a4d
Xuân Tùng <xuantung.1998@gmail.com>
2019-04-23 16:18:23 +0700

    Update All

--- src/PlayList.js
+++ src/PlayList.js
@@ -3,7 +3,7 @@ export default class PlayList {
     static songs = [
         {
             id: '01',
-            url: 'https://aredir.nixcdn.com/NhacCuaTui942/ILikeMeBetter-Lauv-4940820_hq.mp3?st=UyvAdQU-m1uMRotG0uTgGQ&e=1555924063&download=true',
+            url: 'https://aredir.nixcdn.com/NhacCuaTui942/ILikeMeBetter-Lauv-4940820_hq.mp3?st=fiE0UFWIWEwIIjTFd7OLNg&e=1556097437&download=true',
             title: 'I like me better',
             artist: 'Lauv',
             img: 'https://i1.sndcdn.com/artworks-000355509837-e1mdg8-t200x200.jpg',
@@ -11,7 +11,7 @@ export default class PlayList {
         },
         {
             id: '02',
-            url: 'https://aredir.nixcdn.com/NhacCuaTui978/ChuyenCuaTa-GleeGiaThieu-5895245_hq.mp3?st=cmbZIUKNSQZIAkZbRjpZuw&e=1555924203&download=true',
+            url: 'https://aredir.nixcdn.com/NhacCuaTui978/ChuyenCuaTa-GleeGiaThieu-5895245_hq.mp3?st=ID6UMUh620AKw2UUnr4wbQ&e=1556097467&download=true',
             title: 'Chuyện của ta',
             artist: 'Glee Gia Thiều',
             img: 'https://i1.sndcdn.com/artworks-000437493624-scd9p0-t200x200.jpg',
@@ -19,7 +19,7 @@ export default class PlayList {
         },
         {
             id: '03',
-            url: 'https://aredir.nixcdn.com/NhacCuaTui968/NangDem-TruongAn-5618039_hq.mp3?st=hOhPBDuSTlKaDCnoumNpzQ&e=1555924126&download=true',
+            url: 'https://aredir.nixcdn.com/NhacCuaTui968/NangDem-TruongAn-5618039_hq.mp3?st=3-kvWAkN5flWj5PRwyWE_w&e=1556097487&download=true',
             title: 'Nắng đêm',
             artist: 'T.R.I, Trường An',
             img: 'https://i1.sndcdn.com/artworks-000296922480-0s8gm0-t200x200.jpg',
@@ -27,7 +27,7 @@ export default class PlayList {
         },
         {
             id: '04',
-            url: 'https://aredir.nixcdn.com/NhacCuaTui969/BenAyBenNay-LongCao-5097837_hq.mp3?st=lgJF2TRo1WDq24EzNApcrA&e=1555924236&download=true',
+            url: 'https://aredir.nixcdn.com/NhacCuaTui969/BenAyBenNay-LongCao-5097837_hq.mp3?st=SpsJ7_a7EbakFBg54Lp05A&e=1556097506&download=true',
             title: 'Bên ấy bên này',
             artist: 'Long Cao',
             img: 'https://i1.sndcdn.com/artworks-000237130424-0iov41-t200x200.jpg',
